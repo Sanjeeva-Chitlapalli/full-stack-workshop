47d3e7c764762f10828f9a2901278960f8711c4a
Sanjeev Chitlapalli <sanjeevachitlapalli@gmail.com>
2022-04-13 12:52:53 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const [show,setshow]=useState(false)
+  const [userInput,setStuff]=useState({
+    name:"",
+    usn:"",
+    uni:""
+  })
+  const handleNameChange=(e)=>{
+    const newObj={
+      name:e.target.value,
+      usn:userInput.usn,
+      uni:userInput.uni,
+    }
+    setStuff(newObj);
+  }
+  const abc=()=>{ 
+    setshow(true)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='Name' onChange={handleNameChange} value={userInput.name}/>
+      {
+        show
+        ?
+        <h1>{userInput.name}</h1>
+        :
+        null
+      }
+      <button onClick={abc}>show</button>
+      <h1>name: {userInput.name}</h1>
     </div>
   );
 }
